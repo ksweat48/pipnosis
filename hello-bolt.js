b73c8f3b62721153ea16224b1c930002e3cd37a0
ksweat48 <ksweat48@gmail.com>
2025-07-01 12:01:54 -0400

    Bolt Artifact Demo

---
+++ hello-bolt.js
@@ -0,0 +1,4 @@
+// This is a simple demonstration of the Bolt artifact system
+console.log("Hello from Bolt's artifact system!");
+console.log("This file was automatically created and saved to your project.");
+console.log("You can now run it with Node.js.");
