a3f95eb9688c615c574acb66670153f365a0819f
ksweat48 <ksweat48@gmail.com>
2025-07-07 22:03:59 -0400

    Fix MarketAnalysis Component

--- src/components/MarketAnalysis.tsx
+++ src/components/MarketAnalysis.tsx
@@ -372,7 +372,7 @@ export const MarketAnalysis: React.FC<MarketAnalysisProps> = ({
             </span>
           </div>
           {analysisMode === 'api' && !isLoading && !error && safeMarketData.length > 0 && lastUpdated && (
-            <span>Updated: {lastUpdated.toLocaleTimeString()}</span>
+            <span>Updated: {lastUpdate.toLocaleTimeString()}</span>
           )}
         </div>
       </div>
