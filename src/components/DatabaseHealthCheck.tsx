4b548bfe7ed8d52f5c70ef3652787feace1fbba2
ksweat48 <ksweat48@gmail.com>
2025-07-08 20:32:12 -0400

    Deleted DatabaseStatus.tsx

--- src/components/DatabaseHealthCheck.tsx
+++
@@ -1,1 +0,0 @@
-// This file has been removed as it was used for checking Supabase database health
--- src/contexts/AuthContext.tsx
+++
@@ -1,2 +0,0 @@
-// This file has been completely removed as it was primarily for Supabase authentication
-// Any components that depended on this context will need to be updated
