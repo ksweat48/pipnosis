4dadbd05dc7f85d594dc6a5e02fe18ffe61470ca
ksweat48 <ksweat48@gmail.com>
2025-08-02 23:02:13 -0400

    Fix border-border class error

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        border: 'rgb(var(--border) / <alpha-value>)',
+      },
+    },
   },
   plugins: [],
 };
