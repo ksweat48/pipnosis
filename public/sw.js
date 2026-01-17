4473cc2159399d47859041848d31a9b0e1911d6f
ksweat48 <ksweat48@gmail.com>
2026-01-17 17:14:26 -0500

    Updated CRITICAL_CHANGES_REPORT.txt

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 // Pipnosis PWA Service Worker
 // Enables installation and "Add to Home Screen" functionality
 
-const BUILD_VERSION = '1.0.0-mkisg9az';
+const BUILD_VERSION = '1.0.0-mkiv60tx';
 const CACHE_NAME = `pipnosis-v${BUILD_VERSION}`;
 const STATIC_ASSETS = [
   '/',
