3b506460d7e585b37c1e869c950214a0767f754d
ksweat48 <ksweat48@gmail.com>
2026-01-22 15:39:38 -0500

    Updated netlify.toml

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 // Pipnosis PWA Service Worker
 // Enables installation and "Add to Home Screen" functionality
 
-const BUILD_VERSION = '1.0.0-mkphmrew';
+const BUILD_VERSION = '1.0.0-mkpwzgvg';
 const CACHE_NAME = `pipnosis-v${BUILD_VERSION}`;
 const STATIC_ASSETS = [
   '/',
