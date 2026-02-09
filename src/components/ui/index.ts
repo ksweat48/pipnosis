a76d9357836e63b9291e249b287120fdf115d767
ksweat48 <ksweat48@gmail.com>
2026-02-09 14:56:57 -0500

    Deleted eslint.config.js

--- src/components/ui/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as BalanceDisplay } from '../BalanceDisplay';
-export { default as ConnectionStatusIndicator } from '../ConnectionStatusIndicator';
-export { ErrorBoundary } from '../ErrorBoundary';
-export { DatabaseErrorBoundary } from '../DatabaseErrorBoundary';
--- src/lib/index.ts
+++
@@ -1,4 +0,0 @@
-export { supabase, isCurrentUserAdmin, getCurrentUserRole } from './supabase';
-export { logger } from './logger';
-export * from './environment';
-export * from './error-handler';
